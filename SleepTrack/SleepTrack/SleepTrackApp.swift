d0824324e3dd1b4678dc85c0c9a34f4b2389644c
Nick Shan <bshan1@ualberta.ca>
2025-11-08 10:44:55 -0700

    basic app

---
+++ SleepTrack/SleepTrack/SleepTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackApp.swift
+//  SleepTrack
+//
+//  Created by Nick Shan on 2025-11-08.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
