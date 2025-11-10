eb35a230f078cd0b69f57147e0f3d7afeb88c912
Nick Shan <bshan1@ualberta.ca>
2025-11-09 21:37:09 -0700

    removed useless

--- RestfulNight/View/Home/HomeView.swift
+++ RestfulNight/View/Home/HomeView.swift
@@ -74,33 +74,6 @@ struct HomeView: View {
     }
 }
 
-//struct ProgressRing: View {
-//    var progress: Double // Value between 0 and 1
-//    @Binding var value: Double // The number displayed in the middle
-//    
-//    var body: some View {
-//        ZStack {
-//            Circle()
-//                .stroke(Color.gray.opacity(0.3), lineWidth: 20)
-//            
-//            Circle()
-//                .trim(from: 1 - progress, to: 1) // Reverse the trim to make it counterclockwise
-//                .stroke(Color.blue, style: StrokeStyle(lineWidth: 20, lineCap: .round))
-//                .rotationEffect(.degrees(-90)) // Rotate counterclockwise to start from the top
-//            
-//            VStack {
-//                Text("\(Int(value))")
-//                    .font(.largeTitle)
-//                    .fontWeight(.bold)
-//                Text("score")
-//                    .font(.caption)
-//                    .foregroundColor(.gray)
-//            }
-//        }
-//        .frame(width: 150, height: 150) // Adjust size as needed
-//    }
-//}
-
 struct MonthlyView: View {
     @State private var sleepScores: [String: Double] = [:] // Date as key, sleep score as value
     private let deviceID = "exampleDeviceID" // Replace with actual device ID
